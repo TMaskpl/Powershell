45410c124cce517ede370b03a0f39c1fc2ac7306
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2023-05-08 15:03:26 +0200

    Update TMaskScript.ps1

--- TMaskScript.ps1
+++ TMaskScript.ps1
@@ -1,16 +1,23 @@
-$ScriptPath = "C:\Windows\Temp\Scripts\"
+[CmdletBinding()]
+param(
+    [string]$ScriptPath = "C:\Windows\Temp\Scripts\",
+    [string]$log="S:\Script\TMaskPL.log",
+    [string]$share="\\IP\pool-nvm",
+    [string]$user="smb",
+    [string]$pass="haslo",
+)
+
+
+
 
 
 Remove-SmbMapping -LocalPath ("S" + ":") -UpdateProfile -Force -ErrorAction Ignore
 
  
 $net = new-object -ComObject WScript.Network
-$net.MapNetworkDrive("S:", "\\IP\pool-nvm", $false, "smb", "haslo")
-#$ec = $?
-#$ec
+$net.MapNetworkDrive("S:", $share, $false, $user, $pass)
 
-
-Get-Date | Out-File -FilePath "S:\Script\TMaskPL.log" -Append
+Get-Date | Out-File -FilePath $log -Append
 Copy-Item "S:\Script\" -Filter "*.ps1" -Destination $ScriptPath -ErrorAction Ignore
 Copy-Item "S:\Script\" -Filter "*.cmd" -Destination $ScriptPath -ErrorAction Ignore
 Copy-Item "S:\Script\" -Filter "*.py" -Destination $ScriptPath -ErrorAction Ignore
@@ -25,5 +32,3 @@ foreach ($f in $files){
 }
 
 Remove-SmbMapping -LocalPath ("S" + ":") -UpdateProfile -Force -ErrorAction Ignore
-#$ec = $?
-#$ec
