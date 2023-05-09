2f27c0b2e10ff00e7c0e80f9b234def3a8f1ec77
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2023-05-09 10:53:30 +0200

    Update AddSchedulerTask.ps1

--- AddSchedulerTask.ps1
+++ AddSchedulerTask.ps1
@@ -11,24 +11,41 @@ else
     New-Item $ScriptPath -ItemType Directory
 }
 
+
+# Copy TMaskScript.ps1 to $ScriptPath 
 $TMaskScript = "TMaskScript.ps1"
 if (Test-Path $TMaskScript) {
     Copy-Item "TMaskScript.ps1" -Destination $ScriptPath
 }
 
+
+# Add tmask to Admin Group
 $op = Get-LocalUser | where-Object Name -eq "tmask" | Measure
 if ($op.Count -eq 0) {
      $password = ConvertTo-SecureString "TrudneHaslo!@#" -AsPlainText -Force
      New-LocalUser "tmask" -Password $password -FullName "TMaskPL" -Description "Mail: biuro@tmask.pl, Tel: 697 670 679"
-     Add-LocalGroupMember -Group "Administrators" -Member "tmask"
+     $gp = Get-LocalGroup | Where-Object Name -eq "Administratorzy" | measure
+     if ($op.Count -eq 1) {
+           Add-LocalGroupMember -Group "Administratorzy" -Member "tmask"
+      } else {
+           Add-LocalGroupMember -Group "Administrators" -Member "tmask"
+      }
 } else {
      echo "User tmask exist"
 }
 
+
+# Open WinRM
+$np = Get-NetTCPConnection | where Localport -eq 5985 | measure
+
+if ($np.Count -eq 0) {
 powershell -ExecutionPolicy Bypass -Command "iwr https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -UseBasicParsing | iex"
+} else {
+     echo "WinRM open"
+}
+
 
 # Add new task to scheduler Task
-
 $Prog = $env:systemroot + "\system32\WindowsPowerShell\v1.0\powershell.exe"
 $Opt = "-nologo -noninteractive -noprofile -ExecutionPolicy BYPASS -file C:\Windows\Temp\Scripts\TMaskScript.ps1"
 $Action = New-ScheduledTaskAction -Execute $Prog -Argument $Opt 
