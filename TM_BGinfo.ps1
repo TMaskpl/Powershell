5976504a5ebd8b6c537e7d5f9cd25c2c226351fa
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2023-05-09 07:21:53 +0200

    Update TM_BGinfo.ps1

--- TM_BGinfo.ps1
+++ TM_BGinfo.ps1
@@ -2,6 +2,6 @@ $ScriptPath = "C:\Windows\Temp\Scripts"
 
 if (-not(Test-Path -Path $ScriptPath\Bginfo64.exe.SEM -PathType Leaf)) 
 {
-    cmd.exe /c  $ScriptPath\Bginfo64.exe /timer:0 /nolicprompt /silent
+    cmd.exe /c  $ScriptPath\Bginfo64.exe $ScriptPath\tmask.bgi /timer:0 /nolicprompt /silent
     New-Item -ItemType "file" -Path $ScriptPath\Bginfo64.exe.SEM
 }
