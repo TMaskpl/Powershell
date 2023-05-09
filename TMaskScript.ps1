cffc863272d08edaf96c3004ad24bcd4475998df
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2023-05-09 10:58:48 +0200

    Update TMaskScript.ps1

--- TMaskScript.ps1
+++ TMaskScript.ps1
@@ -1,3 +1,13 @@
+##################################################
+#                                                #
+#__author__ = "biuro@tmask.pl"                   #
+#__copyright__ = "Copyright (C) 2023 TMask.pl"   #
+#__license__ = "MIT License"                     #
+#__version__ = "1.0"                             #
+#                                                #
+##################################################
+
+
 [CmdletBinding()]
 param(
     [string]$ScriptPath = "C:\Windows\Temp\Scripts\",
