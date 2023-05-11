183a8ca442d6fc1714d369593e70688eeadca1f3
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2023-05-11 18:49:17 +0200

    Update AddSchedulerTask.ps1

--- AddSchedulerTask.ps1
+++ AddSchedulerTask.ps1
@@ -56,10 +56,10 @@ powershell -ExecutionPolicy Bypass -Command "iwr https://raw.githubusercontent.c
 
 
 # Add new task to scheduler Task
-$Prog = $env:systemroot + "\system32\WindowsPowerShell\v1.0\powershell.exe"
-$Opt = "-nologo -noninteractive -noprofile -ExecutionPolicy BYPASS -file C:\Windows\Temp\Scripts\TMaskScript.ps1"
-$Action = New-ScheduledTaskAction -Execute $Prog -Argument $Opt 
-$Trigger = New-ScheduledTaskTrigger -Daily -DaysInterval 1 -RandomDelay "00:15" -At "01:00"
-$Trigger.Repetition = $(New-ScheduledTaskTrigger -Once -RandomDelay "00:15" -At "01:00" -RepetitionInterval "00:05").Repetition
-$Principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-$Task = Register-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Principal -TaskName "MonitorTMaskPL" -Description "Automation Script every 15 min" -Force
+
+$taskName = "MonitorTMaskPL"
+$scriptPath = "C:\Windows\Temp\Scripts\TMaskScript.ps1"
+$arguments = "-ExecutionPolicy Bypass -File '$scriptPath'"
+
+$taskCmd = "SchTasks /Create /SC HOURLY /TN $taskName /TR ""powershell.exe $arguments"" /F"
+Invoke-Expression -Command $taskCmd
