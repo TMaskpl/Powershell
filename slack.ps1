a44c4f1217d4ce77ecd54822b34f86a0ff1d188e
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2023-07-18 20:39:59 +0200

    Update slack.ps1

--- slack.ps1
+++ slack.ps1
@@ -1,3 +1,13 @@
+##################################################
+#                                                #
+#__author__ = "biuro@tmask.pl"                   #
+#__copyright__ = "Copyright (C) 2023 TMask.pl"   #
+#__license__ = "MIT License"                     #
+#__version__ = "1.0"                             #
+#                                                #
+##################################################
+
+
 $Webhook = "https://hooks.slack.com/services/***************"
 $ContentType= 'application/json'
 $Body = @"
